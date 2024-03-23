3a5b30bcfcb2514955f45d38972e9163f474e13c
luizhenriquelucena <riquelucena@gmail.com>
2024-03-23 13:22:08 -0300

    feat(06): Criação das páginas razor.

---
+++ ContactManager/Pages/ContactDetailsPagecshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactManager.Pages
+{
+    public class ContactDetailsPagecshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
