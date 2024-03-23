a821ba11f356f47a9723e51c072cb8f6afbc5941
luizhenriquelucena <riquelucena@gmail.com>
2024-03-23 11:52:54 -0300

    feat(05): Substituição de alguns arquivos e implementação da controller.

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -7,20 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
-builder.Services.AddScoped<IAddContactsBusiness, AddContactsBusiness>();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped <IContactRepository, ContactRepository>();
+builder.Services.AddScoped <IAddContactsBusiness, AddContactsBusiness>();
+builder.Services.AddScoped <IDeleteContactBusiness, DeleteContactBusiness>();
+builder.Services.AddScoped <IGetAllContactsBusiness, GetAllContactsBusiness>();
+builder.Services.AddScoped <IGetByIdContactBusiness, GetByIdContactBusiness>();
+builder.Services.AddScoped <IUpdateContactBusiness, UpdateContactBusiness>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -31,10 +31,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapRazorPages();
-    endpoints.MapFallbackToPage("/Home/ContactRegistry"); // Verifique se a rota está corretamente configurada aqui
-});
+app.MapRazorPages();
 
 app.Run();
